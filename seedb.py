99735a41b2a4ea5b03ffe4329793e5da26b316bd
mt2tkm <sakaimatsumotot@gmail.com>
2017-04-21 17:53:17 +0900

    '4/21-1'

--- seedb.py
+++ seedb.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
-import time,math
+import time,math,numpy as np
 from config import config_data
-import matplotlib.pyplot as plt, pandas as pd
+import matplotlib.pyplot as plt
 
 class SeeDB:
-    query_time,deviance_time,sort_time = 0,0,0
+    query_time,deviance_time,sort_time,visualization_time = 0,0,0,0
 
     def __init__(self, db,data_set,table,k):
         self.cursor = config_data(db)
@@ -18,6 +18,7 @@ class SeeDB:
 
     def query(self):
         if self.func == 'sum' or '注文金額' in self.attribute1:
+
             query1 = 'select ' + self.attribute2 +',' + self.func + '(' + 'CAST(' + self.attribute1 + ' AS BIGINT' + '))' + ' from ' + self.table
         else:
             query1 = 'select ' + self.attribute2 + ',' + self.func + '(' + self.attribute1 + ')' + ' from ' + self.table
@@ -55,6 +56,7 @@ class SeeDB:
         deviance = 0
 
         a = time.time()
+
         x,y = self.nomalization(x),self.nomalization(y)
         dd = dict()
         for i, j in x:
@@ -64,6 +66,7 @@ class SeeDB:
                 dd[i] = math.fabs(dd[i] - j)
             else:
                 dd[i] = j
+
         for x, dis in dd.items():
             deviance += dis
 
@@ -74,11 +77,11 @@ class SeeDB:
     def cheak_k(self,d):
         if self.attribute1 == '*':
             self.attribute1 = '.'+self.attribute1
-
         if len(self.top_k) == 0:
-            self.top_k[0] = (d,(self.func,self.attribute1,self.attribute2)
+            self.top_k[0] = (d,(self.func,self.attribute1,self.attribute2))
         elif len(self.top_k) < self.k:
-            target = (d, (self.func,self.attribute1, self.attribute2)
+
+            target = (d, (self.func,self.attribute1, self.attribute2))
             flg = -1
             for i, j in self.top_k.items():
                 if j[0] < target[0]:
@@ -86,11 +89,51 @@ class SeeDB:
                     target,flg = j,-1
             self.top_k[len(self.top_k)] = target
         else:
-            target = (d,(self.func,self.attribute1,self.attribute2)
+            target = (d,(self.func,self.attribute1,self.attribute2))
             for i,j in self.top_k.items():
                 if j[0] < target[0]:
                     self.top_k[i] = target
                     target = j
+
+    def visualization(self):
+        n = math.ceil(np.sqrt(self.k))
+        m = math.ceil(self.k / n)
+        fig, axes = plt.subplots(nrows=n, ncols=m, figsize=(10, 8))
+        ii = 0
+        for dis,dt in self.top_k.items():
+
+            self.attribute1, self.attribute2, self.func = dt[1][1].split('.')[1], dt[1][2], dt[1][0]
+            data1, data2 = self.query()
+            data1, data2 = self.nomalization(data1), self.nomalization(data2)
+            x_agre = list()
+            for i, j in data1 + data2:
+                if not i in x_agre:
+                    x_agre.append(i)
+            t1, t2 = dict(data1), dict(data2)
+            x, y1, y2 = [i for i in range(0, len(x_agre))], list(), list()
+            for i in x_agre:
+                if i in t1:
+                    y1.append(t1[i])
+                else:
+                    y1.append(0)
+                if i in t2:
+                    y2.append(t2[i])
+                else:
+                    y2.append(0)
+
+            axes[int(ii/m), ii%m].plot(x, y1, linewidth=2)
+            axes[int(ii/m), ii%m].plot(x, y2, linewidth=2)
+            axes[int(ii/m), ii%m].set_xticks(x)
+            axes[int(ii/m), ii%m].set_xticklabels(x_agre, rotation=30)
+            axes[int(ii/m), ii%m].set_title(ii)
+            axes[int(ii/m), ii%m].set_xlabel(self.attribute2)
+            axes[int(ii/m), ii%m].grid(True)
+
+            ii+=1
+            if ii > self.k:
+                break
+        plt.show()
+        #plt.savefig('sample.png')
 
     def output(self):
         print('================================================================')
@@ -103,20 +146,22 @@ class SeeDB:
         print('Query_time:',self.query_time)
         print('Calcu_deviance_time:',self.deviance_time)
         print('Sort_time:',self.sort_time)
+        print('Visualization_time:',self.visualization_time)
         print('================================================================')
 
-    def visualization(self):
-        print(1)
-
     def main(self):
+
         for self.attribute2, ite in self.data_set.items():
             for self.func, self.attribute1 in ite:
                 d = self.distance()
                 a = time.time()
-                if d != -1:
+                if d != -1 and d<1:
                     self.cheak_k(d)
                 self.sort_time += time.time() - a
-            print(time.time() - self.start)
 
+            print(time.time() - self.start)
+        a = time.time()
+        self.visualization()
+        self.visualization_time = time.time() - a
         self.output()
-        #self.visualization()
+
--- vizualization.py
+++ vizualization.py
@@ -1,7 +1,8 @@
 import pandas as pd, matplotlib.pyplot as plt
 import math
 import numpy as np
-
+from config import config_data
+from fasionec import data
 
 def visualization(self):
     n = math.ceil(np.sqrt(self.k))
@@ -57,46 +58,38 @@ def visualization(self):
     
 
 if __name__ == '__main__':
-    t = np.linspace(-np.pi, np.pi, 1000)
-    
-    x1 = np.sin(2*t)
-    x2 = np.cos(2*t)
-    x3 = x1 + x2
+    db, table, data_set = data()
+    cursor =config_data(db)
+    query = 'select ' + '[Member2].都道府県' + ',' + 'sum' + '(' + 'CAST(' + '[OrderDetail].注文金額' + ' AS BIGINT' + '))' + ' from ' + table
+    query += ' group by ' + '[Member2].都道府県' + ' order by ' + '[Member2].都道府県'
 
-    fig,axes = plt.subplots(nrows=2,ncols=3,figsize=(10,8))
-    
-    axes[0,0].plot(t, x1, linewidth=2)
-    axes[0,0].plot(t, x2, linewidth=2)
-    axes[0,0].set_xticks([-1,0,1])
-    axes[0,0].set_xticklabels(['A','B','C'])
-    axes[0,0].set_title('sin')
-    axes[0,0].set_xlabel('t')
-    axes[0,0].set_ylabel('x')
-    axes[0,0].set_xlim(-np.pi, np.pi)
-    axes[0,0].grid(True)
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 8))
+    m,n = 0,0
+    for i in range(5):
+        cursor.execute(query)
+        data1 = cursor.fetchall()
+        x_agre = list()
+        for i, j in data1:
+            if not i in x_agre:
+                x_agre.append(i)
+        t1,y1 = dict(data1),list()
+        x = [i for i in range(0, len(x_agre))]
+        for i in x_agre:
+            if i in t1:
+                y1.append(t1[i])
+            else:
+                y1.append(0)
 
-    axes[0,1].plot(t, x2, linewidth=2)
-    axes[0,1].set_title('cos')
-    axes[0,1].set_xlabel('t')
-    axes[0,1].set_ylabel('x')
-    axes[0,1].set_xlim(-np.pi, np.pi)
-    axes[0,1].grid(True)
-    
-    axes[0,2].plot(t, x2, linewidth=2)
-    axes[0,2].set_title('cos')
-    axes[0,2].set_xlabel('t')
-    axes[0,2].set_ylabel('x')
-    axes[0,2].set_xlim(-np.pi, np.pi)
-    axes[0,2].grid(True)
-    
-    axes[1,0].plot(t, x3, linewidth=2)
-    axes[1,0].set_title('sin+cos')
-    axes[1,0].set_xlabel('t')
-    axes[1,0].set_ylabel('x')
-    axes[1,0].set_xlim(-np.pi, np.pi)
-    axes[1,0].grid(True)
+        axes[m, n].plot(x, y1, linewidth=2)
 
-    axes[1,1].axis('off')
-    axes[1,2].axis('off')
+        axes[m, n].set_xticks(x)
+        axes[m, n].set_xticklabels(x_agre, rotation=30)
 
+        #axes[m, n].set_title(' rank : ', dt[0], self.func, self.attribute1.split('.')[1])
+        #axes[m, n].set_xlabel(self.attiribute2)
+
+        axes[m, n].grid(True)
     plt.show()
+
+
+
