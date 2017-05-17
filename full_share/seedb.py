9daca25b871b329011fc47450eae79117a412e87
mt2tkm <sakaimatsumotot@gmail.com>
2017-05-17 19:44:56 +0900

    make fullshare, but not do visualizationed

--- full_share/seedb.py
+++ full_share/seedb.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class SeeDB:
-    query_time,deviance_time,sort_time,visualization_time = 0,0,0,0
+    query_time,calc_time,visualization_time = 0,0,0
 
     def __init__(self, db, groupby, table, k, aggregate):
         self.con,self.cursor = config_data(db)
@@ -19,30 +19,53 @@ class SeeDB:
         self.column2, self.where2 = input('2つ目のwhere句の属性を入力してください->'), input('2つ目のwhere句の具体的条件を入力してください->')
 
     def fullshare_query(self):
+        # make fullshare_query
         g_b = str()
         for group in self.groupby:
             g_b += group + ', '
         select = 'select ' + g_b
         for agg in self.aggregate:
-            select = select + 'sum(CAST(' + agg + ' AS BIGINT)) AS ' + agg.split('.')[1] + ', ' + 'count(' + agg + ') AS 個数_' + agg.split('.')[1] + ' , '
+            select = select + 'sum(CAST(' + agg + ' AS BIGINT)) AS ' + agg.split('.')[1] + ', '
         select = select[:-2]
-        
-        full_query = select + 'from ' + self.table + ' group by ' + g_b[:-2] + ' order by ' + g_b[:-2]
-
+        full_query = select + ' from ' + self.table + ' group by ' + g_b[:-2] + ' order by ' + g_b[:-2]
+        #print(full_query)
+        # execute fullshare_query and put in DataFrame
         self.df = pd.io.sql.read_sql(full_query,self.con)
 
     def roop(self):
-        print(len(self.df))
         df1 = self.df
         for i in range(len(self.groupby)):
-            dt1 = df1[df1[self.column1] == self.where1].groupby(df1.columns[0]).sum().iloc[:,-6:]
-        print(dt1)
-        sample = (dt1 - dt1.mean())/dt1.std()
-        print(sample)
+            # whether terms match group-by column
+            if self.column1 != df1.columns[i] and self.column2 != df1.columns[i]:
+                dt1, dt2 = pd.DataFrame(), pd.DataFrame()
+                if self.column1 != '':
+                    dt1 = df1[df1[self.column1] == self.where1].groupby(df1.columns[i]).sum().iloc[:,-3:]
+                else:
+                    dt1 = df1.groupby(df1.columns[i]).sum().iloc[:, -3:]
+                if self.column2 != '':
+                    dt2 = df1[df1[self.column2] == self.where2].groupby(df1.columns[i]).sum().iloc[:, -3:]
+                else:
+                    dt2 = df1.groupby(df1.columns[i]).sum().iloc[:, -3:]
+                """
+                dt1['平均注文金額'] = [dt1.ix[pp, -3] / dt1.ix[pp, -2] for pp in range(len(dt1.ix[:, -2]))]
+                dt2['平均注文金額'] = [dt2.ix[pp, -3] / dt2.ix[pp, -2] for pp in range(len(dt2.ix[:, -2]))]
+                
+                dt1['平均注文時追加金額'] = [dt1.ix[pp, 2] / dt1.ix[pp, 1] for pp in range(len(dt1.ix[:, 1]))]
+                dt2['平均注文時追加金額'] = [dt2.ix[pp, 2] / dt2.ix[pp, 1] for pp in range(len(dt2.ix[:, 1]))]
+                """
+                #dt1.to_csv('test.csv')
+                # z_nomalization phase
+                n_dt1, n_dt2 = (dt1 - dt1.mean())/dt1.std(), (dt2 - dt2.mean())/dt2.std()
+                dev_df = np.fabs(n_dt1 - n_dt2)
 
+                # top-k chaek phase
+                for xx in range(len(dev_df.columns)):
+                    self.cheak(dev_df.ix[:,xx].sum(),dev_df.columns[xx],df1.columns[i])
 
+            else:
+                pass
 
-
+    """
     def nomalization(self,data):
         z,sum_x = tuple(),0
         for x,y in data:
@@ -50,7 +73,8 @@ class SeeDB:
         for x,y in data:
             z += ( (x , y/sum_x), )
         return z
-
+    
+    
     def distance(self):
         a = time.time()
         #x,y = self.query()
@@ -71,10 +95,28 @@ class SeeDB:
         self.deviance_time += time.time() - a
 
         return deviance
+    """
+
+    def cheak(self,dev,y_axis,x_axis):
+        z = (dev,x_axis,y_axis)
+        if len(self.top_k)== 0:
+            self.top_k[0] = z
+        elif len(self.top_k) < self.k:
+            for i,j in self.top_k.items():
+                if j[0] < z[0]:
+                    self.top_k[i] = z
+                    z = j
+            self.top_k[len(self.top_k)] = z
+        else:
+            for i,j in self.top_k.items():
+                if j[0] < z[0]:
+                    self.top_k[i] = z
+                    z = j
 
     def cheak_k(self,d):
         if self.attribute1 == '*':
             self.attribute1 = '.'+self.attribute1
+
         if len(self.top_k) == 0:
             self.top_k[0] = (d,(self.func,self.attribute1,self.attribute2))
         elif len(self.top_k) < self.k:
@@ -92,6 +134,8 @@ class SeeDB:
                 if j[0] < target[0]:
                     self.top_k[i] = target
                     target = j
+
+
 
     def visualization(self):
         n = math.ceil(np.sqrt(self.k))
@@ -138,22 +182,30 @@ class SeeDB:
         print('順位, 乖離度, (集計関数, 集計属性, 集約属性)')
         print('================================================================')
         for i,j in self.top_k.items():
-            print(i+1,j[0],j[1])
+            print(i+1,j[0],j[1],j[2])
         print('================================================================')
         print('All_time:',time.time()-self.start)
         print('Query_time:',self.query_time)
-        print('Calcu_deviance_time:',self.deviance_time)
-        print('Sort_time:',self.sort_time)
+        print('Calculate_time:',self.calc_time)
         print('Visualization_time:',self.visualization_time)
         print('================================================================')
 
 
     def main(self):
+        # query phase
         a = time.time()
         self.fullshare_query()
-        self.query_time += time.time() - a
+        self.query_time = time.time() - a
+        # calculate phase
+        a = time.time()
         self.roop()
-        #self.visualization()
-        #self.visualization_time = time.time() - a
-        #self.output()
+        self.calc_time = time.time() - a
+        """
+        # visualization phase
+        a = time.time()
+        self.visualization()
+        self.visualization_time = time.time() - a
+        """
+        # output phase
+        self.output()
 
